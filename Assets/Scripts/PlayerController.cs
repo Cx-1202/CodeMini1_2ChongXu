0299a82e332e0e76d05f35caa133bdb8db6d76a6
Cx-1202 <chongxu2468@gmail.com>
2020-10-29 13:29:08 +0800

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    int walltouched = 0;
+    int jumpforce = 15;
+    int jumpinit = 0;
     int planeB = -15;
     float speed = 10.0f;
     float zlimit = 9.0f;
-    float xlimit = 9.3f;
-    float minlimit = -4.34f;
-    float maxlimit = 5.95f;
-    float xminlimit = -9.3f;
+    float xlimit = 9.95f;
+    float zminlimit = -4.75f;
+    float zmaxlimit = 3.92f;
+    float xminlimit = -19.54f;
+    float xmaxlimit = -10.1f;
     bool Touch;
     Rigidbody playerRb;
     float gravitymoldifier = 2.5f;
@@ -23,15 +27,22 @@ public class PlayerController : MonoBehaviour
 
         Physics.gravity *= gravitymoldifier;
     }
-  
+
     void Update()
     {
-        
-        
+        jumping();
+
         float verticalInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
 
-        
+        if ( walltouched > 0)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        }
+
+
         if (Touch == false)
         {
             if (transform.position.z > zlimit)
@@ -52,35 +63,41 @@ public class PlayerController : MonoBehaviour
             {
                 transform.position = new Vector3(xlimit, transform.position.y, transform.position.z);
             }
-            else if ( transform.position.x <= xminlimit && transform.position.z < minlimit && transform.position.z > maxlimit )
+            else if (transform.position.x < -xlimit && transform.position.z > 5)
             {
-                transform.position = new Vector3(xminlimit, transform.position.y, transform.position.z);
-                transform.position = new Vector3(transform.position.x, transform.position.y, minlimit);
-                transform.position = new Vector3(transform.position.x, transform.position.y, maxlimit);
+                transform.position = new Vector3(-xlimit, transform.position.y, transform.position.z);
+            }
+            else if (transform.position.x < -xlimit && transform.position.z < -5)
+            {
+                transform.position = new Vector3(-xlimit, transform.position.y, transform.position.z);
             }
             else
             {
                 transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
             }
-
-            /* if (transform.position.z < maxlimit && transform.position.x <= xminlimit)
-             {
-                 transform.position = new Vector3(transform.position.x, transform.position.y, maxlimit);
-             }*/
-
-
-
-
-
-            /*else if (transform.position.x < -xlimit)
+        }
+        else
+        {
+            if (transform.position.z > zmaxlimit)
             {
-                transform.position = new Vector3(-xlimit, transform.position.y, transform.position.z);
-            }*/
-
-            if (transform.position.z < minlimit && transform.position.z > maxlimit && transform.position.x < -xlimit)
+                transform.position = new Vector3(transform.position.x, transform.position.y, zmaxlimit);
+            }
+            else if (transform.position.z < zminlimit)
             {
-                transform.position = new Vector3(transform.position.x, transform.position.y, minlimit);
-                transform.position = new Vector3(transform.position.x, transform.position.y, maxlimit);
+                transform.position = new Vector3(transform.position.x, transform.position.y, zminlimit);
+            }
+            else
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * verticalInput * speed);
+            }
+
+            if (transform.position.x < xminlimit)
+            {
+                transform.position = new Vector3(xminlimit, transform.position.y, transform.position.z);
+            }
+            else
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
             }
         }
     }
@@ -88,13 +105,32 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("PlaneA"))
         {
+            Debug.Log("Plane A");
             Touch = false;
         }
-        else if ( collision.gameObject.CompareTag("PlaneB"))
+        else if (collision.gameObject.CompareTag("PlaneB"))
         {
+            Debug.Log("Plane B");
             Touch = true;
         }
+
+        if (collision.gameObject.CompareTag("wall"))
+        {
+            Debug.Log("Touch wall");
+            walltouched += 1;
+        }
+    }
+    private void jumping()
+    {
+       
+            if (Input.GetKeyDown(KeyCode.Space) && jumpinit < 2)
+            {
+                Debug.Log("Player JUMPING Y Position: " + transform.position.y);
+                playerRb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+
+                jumpinit++;
+            }
+        
     }
 
-
-        }
+}
